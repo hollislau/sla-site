0168b260e100ecd77075ab898aff37a364519b9f
Hollis Lau <hollislau@hotmail.com>
2017-03-04 19:30:51 -0800

    Add index.html file; add gulp build and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ const lintFiles = [
   'server.js'
 ];
 const staticFiles = ['app/**/*.html'];
+var nodemonStream;
 
 gulp.task('lint', () => {
   return gulp.src(lintFiles)
@@ -23,4 +24,38 @@ gulp.task('lint', () => {
     .pipe(eslint.failAfterError());
 });
 
+gulp.task('static:dev', () => {
+  return gulp.src(staticFiles)
+    .pipe(gulp.dest('build'))
+    .pipe(livereload());
+});
+
+gulp.task('static:pro', () => {
+  return gulp.src(staticFiles)
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', ['build:dev'], () => {
+  if (nodemonStream) {
+    nodemonStream.emit('restart');
+  } else {
+    nodemonStream = nodemon({
+      script: 'server.js',
+      watch: ['lib', '_server.js', 'server.js']
+    })
+    .on('restart', () => {
+      process.stdout.write('Server restarted!\n');
+    })
+    .on('crash', () => {
+      process.stderr.write('Server crashed!\n');
+    });
+  }
+
+  livereload.listen();
+  gulp.watch(staticFiles, ['static:dev']);
+  gulp.watch('gulpfile.js', ['watch']);
+});
+
+gulp.task('build:dev', ['static:dev']);
+gulp.task('build:pro', ['static:pro']);
 gulp.task('default', ['lint']);
