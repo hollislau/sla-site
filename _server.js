a6b5e62190af07681e4f91c640837b4dbe253c6a
Hollis Lau <hollislau@hotmail.com>
2017-03-08 23:57:31 -0800

    Add morgan logging; add basic http middleware

--- _server.js
+++ _server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.static(__dirname + '/build'));
 
 module.exports = exports = function (port, mongoDbUri, cb) {
